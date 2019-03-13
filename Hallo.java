d7464200002e8bfddfc9a0204881951130f9c340
AkhmadTantowi <tantowix72@gmail.com>
2019-03-13 21:39:32 +0700

    file pertama

---
+++ Hallo.java
@@ -0,0 +1,7 @@
+public class Hallo{
+    public static void main(String args[]) {
+        System.out.println("Nama ; Akhmad Tantowi");
+        System.out.println("NIM ; 17090031");
+        System.out.println("Kelas ; 4C");
+    }
+}
